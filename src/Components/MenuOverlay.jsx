734c76babfb999f464e4776c4c27d0d3499aede9
Mathias Talo <mathias.talo@outlook.com>
2024-02-06 13:27:54 +0200

    Added switches for dark/light EST/ENG

--- src/Components/MenuOverlay.jsx
+++ src/Components/MenuOverlay.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import { LuSunMedium } from "react-icons/lu";
+
 const MenuOverlay = ({ navbarOpen, setNavbarOpen }) => {
   return (
     <nav
-      className={`fixed flex top-0 right-0 w-full z-14 h-full bg-[#202831] text-[#e8e6ce] bg-opacity-100 transform delay-100 transition-all duration-300 ${
+      className={`fixed flex flex-col justify-between top-0 right-0 w-full z-14 h-full bg-[#202831] text-[#e8e6ce] bg-opacity-100 transform delay-100 transition-all duration-300 ${
         navbarOpen ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"
       }`}
     >
-      <ul className="w-full flex flex-col items-center justify-center">
+      <ul className="w-full flex flex-col items-center justify-center h-full">
         <li className="nav-li text-[4.25rem] max-[490px]:text-[3.5rem] font-semibold font-sourceCodePro">
           <a
             href="/"
@@ -45,6 +47,18 @@ const MenuOverlay = ({ navbarOpen, setNavbarOpen }) => {
           </a>
         </li>
       </ul>
+      <div className="h-16 w-full flex">
+        <div className="h-full w-[50%] flex justify-center items-center">   
+          <button className="font-bold text-xl w-8 h-8 border-[#e8e6ce] border-2 rounded-md flex justify-center items-center">
+            <LuSunMedium />
+          </button>
+        </div>
+        <div className="h-full w-[50%] flex gap-2 justify-center items-center">
+            <p className="font-bold text-xl -mb-1">ENG</p>
+            <p className="font-bold text-xl">|</p>
+            <p className="font-bold text-xl -mb-1">EST</p>
+        </div>
+      </div>
     </nav>
   );
 };
