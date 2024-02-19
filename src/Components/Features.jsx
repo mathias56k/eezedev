d93e01c52e7476651c5f27dcb847647d3085b922
Mathias Talo <mathias.talo@outlook.com>
2024-02-19 21:56:00 +0200

    Finished features sections for now & started making hero section scalable

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -7,42 +7,87 @@ import { GoGraph } from "react-icons/go";
 
 const Features = () => {
   return (
-    <div className="my-10">
-        <div className="bg-red-900 w-full h-[50rem] flex flex-col md:grid md:grid-rows-3 md:grid-cols-2 md:h-auto md:my-16 md:gap-2 xl:grid-cols-3 xl:grid-rows-2">
-            <div className="bg-blue-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <RiMoneyEuroCircleLine className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Affordable</p>
+    <div className="my-20">
+        <div className="w-full flex flex-col md:grid md:grid-rows-3 md:grid-cols-2 md:h-auto md:my-16 md:gap-2 xl:grid-cols-3 xl:grid-rows-2">
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <RiMoneyEuroCircleLine className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Affordable</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>Transform your online presence with our affordable web design and development services, delivering excellence without compromising your budget</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-green-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <IoIosSpeedometer className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Fast</p>
+            <div className="flex justify-center mt-6 mb-4 md:hidden">
+                <hr className="w-[85%] h-1 bg-[#e8e6ce] border-0 rounded-full" />
+            </div>
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <IoIosSpeedometer className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Flexible</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>Enjoy the freedom to pause or cancel your subscription at your convenience</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-yellow-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <IoIosSpeedometer className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Flexible</p>
+            <div className="flex justify-center mt-6 mb-4 md:hidden">
+                <hr className="w-[85%] h-1 bg-[#e8e6ce] border-0 rounded-full" />
+            </div>
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <IoIosSpeedometer className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Fast</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>Swift and efficient solutions tailored to your needs, where client requests are typically completed in three days or less, ensuring speedy delivery of exceptional results</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-orange-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <IoIosSpeedometer className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Easy</p>
+            <div className="flex justify-center mt-6 mb-4 md:hidden">
+                <hr className="w-[85%] h-1 bg-[#e8e6ce] border-0 rounded-full" />
+            </div>
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <IoIosSpeedometer className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Easy</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>Simplify your experience with user-friendly and hassle-free processes. We provide good documentation and fast customer service</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-pink-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <GoGraph className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Fruitful</p>
+            <div className="flex justify-center mt-6 mb-4 md:hidden">
+                <hr className="w-[85%] h-1 bg-[#e8e6ce] border-0 rounded-full" />
+            </div>
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <GoGraph className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Fruitful</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>We specialize in crafting fast-loading, SEO-optimized websites that yield impressive results, ensuring your digital journey is both seamless and successful</p>
+                    </div>
                 </div>
             </div>
-            <div className="bg-green-900 w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
-                <div className="flex items-center w-[45%] justify-left">
-                    <FaLightbulb className="h-10 w-10 mr-4" />
-                    <p className="text-2xl font-bold">Innovative</p>
+            <div className="flex justify-center mt-6 mb-4 md:hidden">
+                <hr className="w-[85%] h-1 bg-[#e8e6ce] border-0 rounded-full" />
+            </div>
+            <div className="w-full h-[20%] flex items-start justify-center pt-2 md:row-span-1 md:col-span-1 md:h-[10rem]">
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center w-[45%] justify-center">
+                        <FaLightbulb className="h-10 w-10 mr-4" />
+                        <p className="text-2xl font-bold">Innovative</p>
+                    </div>
+                    <div className="w-[80%] lg:w-[60%] flex text-center my-2 leading-[1.2rem]">
+                        <p>We leverage the latest and most effective technologies to build your websites, ensuring a modern and efficient online presence</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,16 +4,16 @@ const Hero = () => {
   return (
     <div className="-mt-4 h-[30rem] p-5 md:h-[50rem] md:p-10 md:pt-5">
       <div className="bg-[#e8e6ce] w-full h-full rounded-2xl flex flex-col">
-      <img src="./line.svg" alt="" className="w-[13rem] absolute top-[22.17rem] z-10 right-0" />
-        <div className="h-[50%] w-[85%] md:w-full p-8">
-          <div className="text-5xl font-black text-[#202831] tracking-wide leading-[.9]">
+      <img src="./line.svg" alt="" className="w-[13rem] absolute top-[22.17rem] z-10 right-0 md:w-[23rem] md:top-[33.18rem]" />
+        <div className="h-[50%] w-[83%] md:w-full xl:bg-red-900 p-8">
+          <div className="text-5xl font-black md:text-8xl text-[#202831] tracking-wide leading-[.9]">
             <span className="inline-flex items-center">
               <span>BEAUTIFUL</span>
-              <img src="./star.svg" alt="Beautiful Image" className="w-10 ml-[-1.25rem] mt-[-2rem] rotate-[30deg] animate-" />
-              <img src="./star.svg" alt="Beautiful Image" className="w-4 ml-[-1.5rem] mt-[.5rem] rotate-[20deg]" />
+              <img src="./star.svg" alt="Beautiful Image" className="w-10 md:w-14 md:mt-[-3rem] ml-[-1.25rem] mt-[-2rem] rotate-[30deg] animate-" />
+              <img src="./star.svg" alt="Beautiful Image" className="w-4 md:w-6 ml-[-1.5rem] mt-[.5rem] rotate-[20deg]" />
             </span>
             <span>WEBSITES MADE</span>
-            <span className="text-[#46a2a1] md:ml-2 inline-flex flex-col h-[calc(theme(fontSize.5xl)*0.9)] md:h-[calc(theme(fontSize.5xl)*0.9)] overflow-hidden">
+            <span className="text-[#46a2a1] md:ml-2 inline-flex flex-col h-[calc(theme(fontSize.5xl)*0.9)] md:h-[calc(theme(fontSize.8xl)*0.9)] overflow-hidden">
               <ul className="block animate-text-slide-5 text-left leading-[0.9] [&_li]:block">
                 <li>EASY</li>
                 <li>FAST</li>
@@ -26,7 +26,8 @@ const Hero = () => {
           </div>
         </div>
         <div className="h-[40%] w-[95%] bg-[#344150] rounded-r-[9rem] mt-3 relative">
-          <img src="./star-2.svg" alt="" className="w-24 rotate-12 absolute top-[-2rem] right-[-1rem]" />
+          <img src="./star-2.svg" alt="" className="w-24 rotate-12 absolute top-[-2rem] right-[-1rem] md:w-48 md:top-[-4rem]" />
+          <img src="./character.png" alt="" className="w-48 absolute left-4 top-4 md:w-96 md:top-[-2rem]" />
         </div>
       </div>
     </div>
